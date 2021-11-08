3e1a3255e2dc6bbff738ef80b9a41d07a419dfa6
tarantino23 <tarassukho@gmail.com>
2021-11-08 10:14:13 -0500

    imported json file into beaches collection

--- server/server.js
+++ server/server.js
@@ -10,10 +10,18 @@ app.use(express.urlencoded( {extended: true} ));
 
 
 // Connecting our database
-mongoose.connect('????', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://Tarantino23:Mozarella0423@cluster0.gkacy.mongodb.net/Beach-Bum?retryWrites=true&w=majority', {useNewUrlParser: true});
 mongoose.connection.once('open', function() {
   console.log("MongoDB database connection established successfully");
 });
+
+// Using mongoimport to import data from a JSON into our MongoDB Atlas cluster
+mongoimport --uri "mongodb://:codEsmith001@atlas-host1:27017,atlas-host2:27017,atlas-host3:27017/Beach-Bum?ssl=true&replicaSet=myAtlasRS&authSource=admin" --collection NYbeaches --drop --file /home/tarantino23/Codesmith/BeachBum/Beach-Bum-App/server/jsonFiles/DPR_Beaches_001.json
+
+
+mongoimport --db Beach-Bum --collection NYbeaches --authenticationDatabase admin --username Tarantino23 --password codEsmith001 --drop --file /home/tarantino23/Codesmith/BeachBum/Beach-Bum-App/server/jsonFiles/DPR_Beaches_001.json
+
+--authenticationDatabase admin
 
 //Render the landing page
 app.get('/', (req, res) => {
